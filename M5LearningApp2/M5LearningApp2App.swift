9f0a96a02c979a2b23c2e802f345775bceb7b8b2
Ray Jamp <rayjamp@gmail.com>
2023-07-19 07:03:39 +0800

    Initial Commit

---
+++ M5LearningApp2/M5LearningApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  M5LearningApp2App.swift
+//  M5LearningApp2
+//
+//  Created by Rueiming Jamp on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct M5LearningApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

27f734d86796ef493348e7b1a2712c7ed20ba2ac
Ray Jamp <rayjamp@gmail.com>
2023-07-19 16:09:42 +0800

    parse json file and style sheet

--- M5LearningApp2/ViewModels/ContentModel.swift
+++ M5LearningApp2/ViewModels/ContentModel.swift
@@ -8,5 +8,48 @@
 import Foundation
 
 class ContentModel: ObservableObject {
+    @Published var modules = [Module]()
+    var styleData: Data?
     
+    init() { getLocalData() }
+    
+    func getLocalData() {
+        //Get a url to the json file
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            // Read the file into a data object
+            let jsonData = try Data(contentsOf: jsonUrl!)
+            let testString = String(decoding: jsonData, as: UTF8.self)
+            print(testString.count)
+//            print(testString.prefix(10000))
+
+            //Try to decode the json into an array of modules
+            let jsonDecoder = JSONDecoder()
+            let modules = try jsonDecoder.decode([Module].self, from: jsonData)
+            // Assign parsed modules to modules property
+            self.modules = modules
+            print(modules)
+        }
+        catch let error {
+            // TODO log error
+            print("Error in jsonDcoder: \(error)")
+        }
+        
+        // Parse the style data
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        do {
+            // Read the file into a data object
+            let styleData = try Data(contentsOf: styleUrl!)
+        
+            // Assign parsed modules to modules property
+            self.styleData = styleData
+            print(styleData)
+        }
+        catch let error {
+            // TODO log error
+            print("Error in style data: \(error)")
+        }
+        
+    }
 }
--- M5LearningApp2/Views/HomeView.swift
+++ M5LearningApp2/Views/HomeView.swift
@@ -10,18 +10,53 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var model: ContentModel
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        ScrollView {
+            LazyVStack {
+                ForEach(model.modules) { module in
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                            .aspectRatio(CGSize(width: 335, height: 175), contentMode: .fit)
+                        HStack {
+                            // Image
+                            Image("swift")
+                                .resizable()
+                                .frame(width: 115, height: 115)
+                                .clipShape(Circle())
+                            Spacer()
+                            // Text
+                            VStack(alignment: .leading, spacing: 10) {
+                                // Headline
+                                Text("lean Swift").bold()
+                                // Description
+                                Text("some description").padding(.bottom, 20)
+                                // Icons
+                                HStack {
+                                    // Number of lessons/questions
+                                    Image(systemName: "text.book.closed")
+                                        .resizable().frame(width:15, height:15)
+                                    Text("20 Lessons").font(.caption)
+                                    //Time
+                                    Image(systemName: "clock")
+                                        .resizable().frame(width:15, height: 15)
+                                    Text("2 Hours").font(.caption)
+                                }
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                    }
+                }
+            }
+            .padding()
+        } }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
+        
     }
 }
